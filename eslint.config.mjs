de15084056aa80d5f6c5990e9d483877140dd45b
isha <ishasagar144@gmail.com>
2026-03-17 17:58:33 +0530

    add build step

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,22 @@
 import js from "@eslint/js";
 import globals from "globals";
+import security from "eslint-plugin-security";
 
 export default [
   {
     files: ["**/*.js"],
+    plugins: {
+      security
+    },
     languageOptions: {
       sourceType: "commonjs",
       globals: {
         ...globals.node,
         ...globals.jest
       }
+    },
+    rules: {
+      ...security.configs.recommended.rules
     }
   },
   js.configs.recommended
